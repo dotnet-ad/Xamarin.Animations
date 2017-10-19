09419e61305b3199669543e2b894d8a6039ac86a
Ned Lanon <ned.lanon@outlook.com>
2017-10-19 12:15:06 +0100

    Initialized Windows Universal App version.

--- Xamarin.Animations/Xamarin.Animations.Shared/AnimationDirection.cs
+++ Xamarin.Animations/Xamarin.Animations.Shared/AnimationDirection.cs
@@ -2,6 +2,8 @@
 namespace Xamarin.Animations.Droid
 #elif __UNIFIED__
 namespace Xamarin.Animations.iOS
+#elif __W_UNIVERSAL__
+namespace Xamarin.Animations.Win.Universal
 #endif
 {
     /// <summary>
---
+++ Xamarin.Animations/Xamarin.Animations.Win.Universal/AnimationExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Xamarin.Animations.Win.Universal
+{
+    public static class AnimationExtensions
+    {
+        /// <summary>
+        /// Fades the view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Fade(this UIElement view, AnimationTransition transition, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Flips the view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="direction">Direction.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Flip(this UIElement view, AnimationTransition transition, AnimationDirection direction, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+           
+        }
+
+        /// <summary>
+        /// Rotates the view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="direction">Direction.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Rotate(this UIElement view, AnimationTransition transition, AnimationDirection direction, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Scales the view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Scale(this UIElement view, AnimationTransition transition, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Slides the view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="direction">Direction.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Slide(this UIElement view, AnimationTransition transition, AnimationDirection direction, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+           
+        }
+
+        /// <summary>
+        /// Zooms the specified view over time.
+        /// </summary>
+        /// <param name="view">View.</param>
+        /// <param name="transition">Transition.</param>
+        /// <param name="duration">Duration.</param>
+        /// <param name="onFinished">On finished.</param>
+        public static void Zoom(this UIElement view, AnimationTransition transition, double duration = AnimationConstants.DefaultDuration, Action onFinished = null)
+        {
+           
+        }
+    }
+}
